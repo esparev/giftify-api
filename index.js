69a59bc0623928da85645bf08f0e241deaf73294
JoseMa <esparev@hotmail.com>
2023-04-21 15:21:25 -0600

    refactor: ♻️ separate the create server app responsibility

--- index.js
+++ index.js
@@ -1,43 +1,7 @@
-const express = require('express');
-const useGraphQL = require('./api/graphql');
-const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
-const passport = require('passport');
-const routerAuth = require('./auth');
-
-require('./auth');
-
-const {
-	errorHandler,
-	ormErrorHandler,
-	boomErrorHandler,
-} = require('./middlewares/error.handler');
-
-const app = express();
+const createApp = require('./app');
 const port = process.env.PORT || 3003;
 
-const limiter = rateLimit({
-	windowMs: 15 * 1000, // 15 seconds
-	max: 100, // limit each IP to 100 requests per windowMs
-});
-
-app.use(limiter);
-app.use(passport.initialize());
-app.use(express.json());
-app.use(bodyParser.json({ limit: '5mb' }));
-
-app.get('/', (req, res) => {
-	res.send("Esparev's API");
-});
-
-routerAuth(app);
-(async () => {
-	await useGraphQL(app);
-})();
-
-app.use(errorHandler);
-app.use(ormErrorHandler);
-app.use(boomErrorHandler);
+const app = createApp();
 
 app.listen(port, () =>
 	console.log(`Server running on http://localhost:${port}`)
