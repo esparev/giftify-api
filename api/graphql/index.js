a9f2f9f9b9980b8ce04f1d3a00b98905b8872549
JoseMa <esparev@hotmail.com>
2023-04-20 22:26:40 -0600

    remove: 🔥 remove graphql authentication method

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -3,7 +3,6 @@ const {
 	ApolloServerPluginLandingPageGraphQLPlayground,
 } = require('apollo-server-core');
 const { loadFiles } = require('@graphql-tools/load-files');
-const { buildContext } = require('graphql-passport');
 const resolvers = require('./resolvers/resolvers');
 
 /**
@@ -15,7 +14,6 @@ const useGraphQL = async (app) => {
 		typeDefs: await loadFiles('./api/graphql/**/*.graphql'),
 		resolvers,
 		playground: true,
-		context: ({ req, res }) => buildContext({ req, res }),
 		plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
 	});
 
