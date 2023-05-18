660f75b5edbf7dfe4ea86783ccb4bd4620d5e04e
JoseMa <esparev@hotmail.com>
2023-05-18 16:17:19 -0600

    feat: ✨ filter gifts by category and search input

--- api/graphql/resolvers/resolvers.js
+++ api/graphql/resolvers/resolvers.js
@@ -1,7 +1,7 @@
 const { address, addresses, allAddresses, createAddress, updateAddress, deleteAddress } = require('./address.resolvers'); // prettier-ignore
 const { cart, carts, createCart, updateCart, deleteCart } = require('./cart.resolvers'); // prettier-ignore
 const { category, categories, createCategory, updateCategory, deleteCategory } = require('./category.resolvers'); // prettier-ignore
-const { gift, gifts, createGift, updateGift, deleteGift } = require('./gift.resolvers'); // prettier-ignore
+const { gift, gifts, giftsByCategory, giftsBySearchInput, createGift, updateGift, deleteGift } = require('./gift.resolvers'); // prettier-ignore
 const { order, orders, allOrders, createOrder, updateOrder, deleteOrder } = require('./order.resolvers'); // prettier-ignore
 const { paymentMethod, paymentMethods, allPaymentMethods, createPaymentMethod, updatePaymentMethod, deletePaymentMethod } = require('./payment-method.resolvers'); // prettier-ignore
 const { user, users, createUser, updateUser, deleteUser } = require('./user.resolvers'); // prettier-ignore
@@ -12,7 +12,7 @@ const resolvers = {
     address, addresses, allAddresses,
     cart, carts,
     category, categories,
-    gift, gifts,
+    gift, gifts, giftsByCategory, giftsBySearchInput,
     order, orders, allOrders,
     paymentMethod, paymentMethods, allPaymentMethods,
     user, users,
