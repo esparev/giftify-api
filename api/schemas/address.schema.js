6e92e5bff271b1b69bce20906301c7d725ffd04f
JoseMa <esparev@hotmail.com>
2023-05-22 17:03:46 -0600

    patch: 🩹 add missing properties in address schema

--- api/schemas/address.schema.js
+++ api/schemas/address.schema.js
@@ -5,9 +5,11 @@ const id = Joi.string().uuid({ version: 'uuidv4' });
 const streetName = Joi.string().max(100);
 const streetNumber = Joi.string().max(10);
 const postalCode = Joi.string().max(10);
-const locality = Joi.string().max(100);
+const city = Joi.string().max(100);
 const area = Joi.string().max(100);
+const locality = Joi.string().max(100);
 const country = Joi.string().max(100);
+const userId = Joi.string().uuid({ version: 'uuidv4' });
 
 const getAddressSchema = Joi.object({
 	id: id.required(),
@@ -17,17 +19,20 @@ const createAddressSchema = Joi.object({
 	streetName: streetName.required(),
 	streetNumber: streetNumber.required(),
 	postalCode: postalCode.required(),
-	locality: locality.required(),
+	city: city.required(),
 	area: area.required(),
+	locality: locality.required(),
 	country: country.required(),
+	userId: userId.required(),
 });
 
 const updateAddressSchema = Joi.object({
 	streetName,
 	streetNumber,
 	postalCode,
-	locality,
+	city,
 	area,
+	locality,
 	country,
 });
 
